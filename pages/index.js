c7bf893e8cd22c7ae6944fd0d6489f6f0d0e05c5
samer hak <samerhak@samers-MacBook-Pro.local>
2020-12-02 22:01:59 -0500

    portfolio added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
 {"title":"index", "fname":"index.md"},
-{"title":"Contact", "fname":"contact.md"}];
+{"title":"Portfolio", "fname":"portfolio.md"},
+{"title":"Contact", "fname":"contact.md"}]
+;

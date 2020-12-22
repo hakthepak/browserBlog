d24a654de33fe0267e46e6645f0ce7bf95027012
samer hak <samerhak@samers-MacBook-Pro.local>
2020-12-21 20:18:53 -0500

    resumpng

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/samres.png"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/p1.png"},
 {"id": "item2", "fname":"item3.md", "specialImage":"/img/p2.png"},
 {"id": "item3", "fname":"item4.md", "specialImage":"/img/p3.png"},
